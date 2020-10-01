e291f75360a43e7d86fad5e85e8b4450e0ef8bc1
ahadee <bagus.ardhika@gmail.com>
2020-10-01 18:58:45 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+
+const App = () => {
+  return (
+    <WebView source={{ uri: 'https://sertifikasiku.com/mobile' }} />
+  );
+};
+
+
+export default App;
